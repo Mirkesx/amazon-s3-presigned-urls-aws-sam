979482a4573eb48cd6ff7c8dac51efce4d22d26f
Danilo <pillitteridanilo@gmail.com>
2022-06-13 11:07:06 +0200

    Update lambda function to sns-topic

--- createArtifact/app.js
+++ createArtifact/app.js
@@ -22,28 +22,28 @@ const s3 = new AWS.S3();
 const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
 // Change this value to adjust the signed URL's expiration
-const URL_EXPIRATION_SECONDS = 300
+const URL_EXPIRATION_SECONDS = 300;
 const suffix_start = 1; 
 const suffix_end = 10;
 
 // Main Lambda entry point
-exports.handler = async (event) => {
+exports.handler = async  (event) => {
   return await createArtifact(event)
-}
+};
 
 const createArtifact = async function (event) {
-  console.log("EVENT: ", JSON.stringify(event))
+  console.log("EVENT: ", JSON.stringify(event));
   const records = event["Records"];
 
   for(let record of records) {
-    //console.log("RECORD: ", record);
+    console.log("RECORD: ", record);
     let s3Params = {
       Bucket: record["s3"]["bucket"]["name"],
       Key: record["s3"]["object"]["key"]
-    }
-    //console.log("PARAMS: ", JSON.stringify(s3Params))
+    };
+    console.log("PARAMS: ", JSON.stringify(s3Params));
     let metaData = await s3.headObject(s3Params).promise();
-    //console.log("metaData: ", JSON.stringify(metaData))
+    console.log("metaData: ", JSON.stringify(metaData));
 
     // Store in a variable called partition a random integer between suffix_start and suffix_end
     let partition = Math.floor(Math.random() * (suffix_end - suffix_start + 1)) + suffix_start;
@@ -58,14 +58,40 @@ const createArtifact = async function (event) {
       original_name: {S: metaData["Metadata"]['original_name']},
       path: {S: s3Params.Key},
       size: {N: `${metaData["ContentLength"]}`},
-    }
+    };
     console.log("ITEM: ", item);
     let params = {
       TableName: `Artifact-${metaData["Metadata"]['tenantid']}`,
       Item: item
     };
     let response = await ddb.putItem(params).promise();
-    //console.log("RESPONSE: ", response);
+    console.log("RESPONSE: ", response);
+    
+    let filename = metaData["Metadata"]['original_name'].split("/");
+    
+    filename = filename[filename.length-1];
+    
+    let messageJson = {
+      "s3Name": s3Params.Bucket,
+      "dowloadKey" : s3Params.Key,
+      "uploadKey" : `digests/${metaData["Metadata"]['tenantid']}/${filename}`,
+      "outgoingMetadata" : {
+            "tenant-id":  `${metaData["Metadata"]['tenantid']}`,
+            "username": `${metaData["Metadata"]['username']}`
+      }
+    };
+    await publishToSNSTopic(messageJson);
   }
+  
   return records;
-}
+};
+
+const publishToSNSTopic = async function(messageJson){
+    const sns = await new AWS.SNS();
+    
+    var params = {
+        Message: JSON.stringify(messageJson), 
+        TopicArn: "arn:aws:sns:eu-north-1:640339490701:s3-event"
+    };
+    await sns.publish(params).promise();
+  };
